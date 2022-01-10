e46026f1dcc9f9476b18f4c9b03cafbaab4e067c
Etienne Chauchot <echauchot@apache.org>
2022-01-10 12:41:54 +0100

    Initial commit

---
+++ src/main/java/org/example/DataStreamJob.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.Encoder;
+import org.apache.flink.api.connector.source.lib.NumberSequenceSource;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the tutorials and examples on the <a
+ * href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run 'mvn clean package' on the
+ * command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class DataStreamJob {
+
+  public static void main(String[] args) throws Exception {
+    final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    final DataStreamSource<Long> dataSource =
+        env.fromSource(
+            new NumberSequenceSource(0, 10000),
+            WatermarkStrategy.noWatermarks(),
+            "streaming source");
+    FileSink<Long> fileSink =
+        FileSink.forRowFormat(
+                new Path("/tmp", "out"), (Encoder<Long>) (element, stream) -> {
+                  stream.write(element.byteValue());
+                  stream.flush();
+                  stream.close();
+                })
+            .build();
+
+    dataSource.sinkTo(fileSink);
+
+    env.setParallelism(4);
+    env.execute("Flink Java API Skeleton");
+  }
+}
